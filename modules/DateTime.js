5f73911193bb8ef54c7eb76f8219e98cdc9018ba
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-07-18 22:34:32 +0545

    Fix eslint error

--- modules/DateTime.js
+++ modules/DateTime.js
@@ -1,9 +1,9 @@
-import { DateTime } from "../node_modules/luxon/src/luxon.js";
+import { DateTime } from '../node_modules/luxon/src/luxon.js';
 
 const date = () => {
   const dt = DateTime.local();
-  const formattedDate = dt.toFormat("EEE MMM dd yyyy h:mm:ss a");
-  const dateTime = document.getElementById("date");
+  const formattedDate = dt.toFormat('EEE MMM dd yyyy h:mm:ss a');
+  const dateTime = document.getElementById('date');
   dateTime.innerHTML = formattedDate;
 };
 
